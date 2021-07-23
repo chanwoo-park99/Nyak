bb4ae513474a92c7e0366453847524ee0f1a5ba7
chanwoo-park99 <woo9903111@gmail.com>
2021-07-23 12:00:57 +0900

    Create main(2).py

---
+++ main(2).py
@@ -0,0 +1,36 @@
+message = "Hello, World!"
+print(message)
+
+print(325 > 325)
+print(325 != 325)
+print(0 == 0)
+print("abc" <= "abc")
+print("323" != "323")
+
+my_age = 20
+my_money = 10000
+print(my_age > 19)
+print(my_money >= 25000)
+print(my_age >= 19 and my_money >= 25000)
+
+weather = "맑음"
+print("비가 내리는가?", weather == "비")
+if weather == "비":
+  print("우산을 가져간다")
+else:
+  print("우산을 가져가지 않는다")
+
+opening = 54700
+closing = 55700
+diff = closing - opening
+if diff > 0:
+  pass
+  pass
+elif diff < 0:
+  pass
+  pass
+else:
+  pass
+  print(diff)
+print("종료!")
+
